c3cd40a1b4b0b15f009704b114dd194f6e8b2ee0
logan-credera <logan.harless@credera.com>
2020-12-20 05:08:25 -0500

    processing

--- Day20/Day20.py
+++ Day20/Day20.py
@@ -1,13 +1,21 @@
 # --- Day 20: Jurassic Jigsaw ---
-# The high-speed train leaves the forest and quickly carries you south. You can even see a desert in the distance! Since you have some spare time, you might as well see if there was anything interesting in the image the Mythical Information Bureau satellite captured.
+# The high-speed train leaves the forest and quickly carries you south. You can even see a desert in the distance!
+# Since you have some spare time, you might as well see if there was anything interesting in the image the Mythical
+# Information Bureau satellite captured.
 #
-# After decoding the satellite messages, you discover that the data actually contains many small images created by the satellite's camera array. The camera array consists of many cameras; rather than produce a single square image, they produce many smaller square image tiles that need to be reassembled back into a single image.
+# After decoding the satellite messages, you discover that the data actually contains many small images created by
+# the satellite's camera array. The camera array consists of many cameras; rather than produce a single square image,
+# they produce many smaller square image tiles that need to be reassembled back into a single image.
 #
-# Each camera in the camera array returns a single monochrome image tile with a random unique ID number. The tiles (your puzzle input) arrived in a random order.
+# Each camera in the camera array returns a single monochrome image tile with a random unique ID number. The tiles
+# (your puzzle input) arrived in a random order.
 #
-# Worse yet, the camera array appears to be malfunctioning: each image tile has been rotated and flipped to a random orientation. Your first task is to reassemble the original image by orienting the tiles so they fit together.
+# Worse yet, the camera array appears to be malfunctioning: each image tile has been rotated and flipped to a random
+# orientation. Your first task is to reassemble the original image by orienting the tiles so they fit together.
 #
-# To show how the tiles should be reassembled, each tile's image data includes a border that should line up exactly with its adjacent tiles. All tiles have this border, and the border lines up exactly when the tiles are both oriented correctly. Tiles at the edge of the image also have this border, but the outermost edges won't line up with any other tiles.
+# To show how the tiles should be reassembled, each tile's image data includes a border that should line up exactly with
+# its adjacent tiles. All tiles have this border, and the border lines up exactly when the tiles are both oriented
+# correctly. Tiles at the edge of the image also have this border, but the outermost edges won't line up with any other tiles.
 #
 # For example, suppose you have the following nine tiles:
 #
@@ -160,3 +168,50 @@
 # To check that you've assembled the image correctly, multiply the IDs of the four corner tiles together. If you do this with the assembled tiles from the example above, you get 1951 * 3079 * 2971 * 1171 = 20899048083289.
 #
 # Assemble the tiles into an image. What do you get if you multiply together the IDs of the four corner tiles?
+
+def get_left(image):
+    lst = image.split()
+    left_list = list()
+    for char in lst[0]:
+        left_list.append(char)
+    return left_list
+
+
+def get_right(image):
+    lst = image.split()
+    left_list = list()
+    for char in lst[-1]:
+        left_list.append(char)
+    return left_list
+
+
+def get_bottom(image):
+    lst = image.split()
+    bottom = lst[-1]
+    bottom_list = list()
+    for char in bottom:
+        bottom_list.append(char)
+    return bottom_list
+
+
+def get_top(image):
+    lst = image.split()
+    bottom = lst[0]
+    bottom_list = list()
+    for char in bottom:
+        bottom_list.append(char)
+    return bottom_list
+
+
+
+class tile:
+    def __init__(self, id, image):
+        self.id = id
+        self.left_side = get_left(image)
+        self.right_side = get_right(image)
+        self.bottom_side = get_bottom(image)
+        self.top_side = get_top(image)
+        self.left_side_flipped = reversed(self.left_side)
+        self.right_side_flipped = reversed(self.right_side)
+        self.bottom_side_flipped = reversed(self.bottom_side)
+        self.top_side = reversed(self.top_side)
